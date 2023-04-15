f2bdac985de41ea2910460fe7bfe6bd987f72875
T.Sanyi <s.truczkai@gmail.com>
2023-04-15 23:33:58 +0200

    implement App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import {useState, useEffect} from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [orders, setOrders] = useState();
+    const [products, setProducts] = useState();
+    const [components, setComponents] = useState();
+    const [storageUnits, setStorageUnits] = useState();
+
+    const getOrders = async () => {
+        try {
+            const response = await api.get("/orders");
+            setOrders(response.data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const getProducts = async () => {
+        try {
+            const response = await api.get("/products");
+            setProducts(response.data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const getComponents = async  () => {
+        try {
+            const response = await api.get("/components");
+            setComponents(response.data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const getStorageUnits = async () =>{
+        try {
+            const response = await api.get("/storage");
+            setStorageUnits(response.data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+
+    useEffect(() => {
+        getOrders();
+        getProducts();
+        getComponents();
+        getStorageUnits();
+    }, [])
+
+    return (
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route path="/" element={<Home/>}></Route>
+                    <Route path="/orders" element={orders}></Route>
+                    <Route path="/products" element={products}></Route>
+                    <Route path="/components" element={components}></Route>
+                    <Route path="/storage" element={storageUnits}></Route>
+                </Route>
+            </Routes>
+            <div className="orders" ></div>
+        </div>
+    );
 }
 
 export default App;
