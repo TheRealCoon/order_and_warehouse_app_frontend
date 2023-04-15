b518ca9e65b1c07d0e91b61612b9833696de70e3
T.Sanyi <s.truczkai@gmail.com>
2023-04-15 23:35:07 +0200

    add more content to Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            Welcome!
+        <div className="container text-center">
+            <div className="content-box d-flex align-items-center display-1">
+                Order and Warehouse Managing Application
+            </div>
+            <div className="display-3">Welcome!</div>
         </div>
     )
 }
