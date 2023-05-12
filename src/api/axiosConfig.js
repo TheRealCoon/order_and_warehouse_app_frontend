3a6c716738abb33a4ab084c090cd26535aa2d675
T.Sanyi <s.truczkai@gmail.com>
2023-05-12 19:58:58 +0200

    change baseURL to remote backend's url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://129.151.211.224/',
+    // baseURL: 'http://localhost:8080',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
